57894548587c07f0ab16cccaac38228fc1237a84
OmidSargazi <om.sargazi@gmail.com>
2024-04-12 14:22:12 +0330

    Big Int with Java

--- Main.java
+++ Main.java
@@ -1,18 +1,19 @@
 public class Main {
     public static void main(String[] args) {
         // System.out.println("Hello");
-        BigInt x = new BigInt("12");
-        BigInt y = new BigInt("13");
-        BigInt z = new BigInt();
-        z.setSum(x, y);
-        // y.Set("15935755");
-        BigInt p = new BigInt();
-        p.setSum("159", "357");
-        p.setSum(x, "357");
-        p.setSum("1598", y);
-        System.out.println(x.Get());
-        System.out.println(y.Get());
-        System.out.println(z.Get());
-        System.out.println(p.Get());
+        // BigInt x = new BigInt("12");
+        // BigInt y = new BigInt("13");
+        // BigInt z = new BigInt();
+        // z.setSum(x, y);
+        // // y.Set("15935755");
+        // BigInt p = new BigInt();
+        // p.setSum("159", "357");
+        // p.setSum(x, "357");
+        // p.setSum("1598", y);
+        // System.out.println(x.Get());
+        // System.out.println(y.Get());
+        // System.out.println(z.Get());
+        // System.out.println(p.Get());
+
     }
 }
