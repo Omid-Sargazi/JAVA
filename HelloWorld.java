459c2035983ffb979e7ac2a3e91fb3bc6170fbde
OmidSargazi <om.sargazi@gmail.com>
2024-04-23 12:16:54 +0330

    transform Base ten into Two

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,21 @@
-class HelloWorld{  
-    public static void main(String args[]){  
-     System.out.println("Hello World");  
-    }  
+import java.util.Scanner;
+
+class HelloWorld {
+    public static void main(String args[]) {
+        System.out.println("Hello World");
+        BaseTwo();
+    }
+
+    public static void BaseTwo() {
+        int a = 12;
+        int s = 0;
+        int p = 1;
+        while (a > 0) {
+            s = s + a % 2 * p;
+            a = a / 2;
+            p = p * 10;
+        }
+        System.out.println(s);
+    }
+
 }
