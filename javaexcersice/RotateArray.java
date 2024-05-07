91ec8f78981ddb0d6e14c6207ae09fc98af7b6f5
OmidSargazi <om.sargazi@gmail.com>
2024-05-07 12:49:20 +0330

    Rotate Left once in Array

--- javaexcersice/RotateArray.java
+++ javaexcersice/RotateArray.java
@@ -3,7 +3,11 @@ package javaexcersice;
 class RotateArray {
     public static void main(String[] args) {
         System.out.println("" + "Rotate Array");
+        rotateLeft();
 
+    }
+
+    public static void rotateRight() {
         int A[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 11, 11, 13, 14, 112, 112, 113, 1000 };
         int n = A.length;
         int temp = 0;
@@ -15,6 +19,20 @@ class RotateArray {
 
         for (int x = 0; x <= n - 1; x++) {
             System.out.print("" + A[x] + ",");
+        }
+    }
+
+    public static void rotateLeft() {
+        int A[] = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23 };
+        int n = A.length;
+        int temp = 0;
+        temp = A[0];
+        for (int i = 0; i < n - 1; i++) {
+            A[i] = A[i + 1];
+        }
+        A[n - 1] = temp;
+        for (int i = 0; i <= n - 1; i++) {
+            System.out.print("A[" + i + "]:" + A[i] + " ");
         }
     }
 }
