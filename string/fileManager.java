2245857dfb66e39f4d2eaa3abd6d665139cc4fc1
OmidSargazi <om.sargazi@gmail.com>
2024-05-20 21:51:31 +0330

    Write in a file in java

---
+++ string/fileManager.java
@@ -0,0 +1,23 @@
+package string;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+
+public class fileManager {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+        File f1 = new File("./a1.txt");
+        try {
+            PrintWriter outfile = new PrintWriter(f1);
+            outfile.print(" Hi Omid");
+            outfile.print("\n" + " Hi World");
+            outfile.close();
+        } catch (FileNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+        System.out.println("" + f1.listFiles());
+    }
+}
