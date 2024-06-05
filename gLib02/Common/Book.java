c6a66c4498171b9ab69e6be9bed2afa676b8ae8a
Omid Sargazi <om.sargazi@gmail.com>
2024-06-06 01:27:05 +0330

    define setter and getter in book class

---
+++ gLib02/Common/Book.java
@@ -0,0 +1,50 @@
+package gLib02.Common;
+
+public class Book {
+    private String BNO;
+    private String Title;
+    private String Author;
+    private int pages;
+    private String subject;
+
+    public void setBNO(String bNO) {
+        BNO = bNO;
+    }
+
+    public void setTitle(String title) {
+        Title = title;
+    }
+
+    public void setAuthor(String author) {
+        Author = author;
+    }
+
+    public void setPages(int pages) {
+        this.pages = pages;
+    }
+
+    public String getBNO() {
+        return BNO;
+    }
+
+    public String getTitle() {
+        return Title;
+    }
+
+    public String getAuthor() {
+        return Author;
+    }
+
+    public int getPages() {
+        return pages;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+}
