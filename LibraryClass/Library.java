787fe9fb9671bcea648accda48c14a87940d0a29
Omid Sargazi <om.sargazi@gmail.com>
2024-06-10 19:30:44 +0330

    create login and registerUser method in Library

---
+++ LibraryClass/Library.java
@@ -0,0 +1,41 @@
+package LibraryClass;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Library {
+    private Map<String, User> users;
+    private Map<String, Admin> admins;
+    private Map<String, Librarian> librarians;
+    private Map<Integer, User> loggedInUsers;
+
+    public Library() {
+        users = new HashMap<>();
+        admins = new HashMap<>();
+        librarians = new HashMap<>();
+        loggedInUsers = new HashMap<>();
+    }
+
+    // User Management
+    public User registerUser(String username, String password, String email) {
+        if (users.containsKey(username)) {
+            System.out.println("Username already exists.");
+            return null;
+        }
+        User user = new User(username, email, password);
+        users.put(username, user);
+        System.out.println("User registered successfully.");
+        return user;
+    }
+
+    public void login(String username, String password) {
+        User user = users.get(username);
+        if (user != null && user.getPassword().equals(password)) {
+            loggedInUsers.put(user.getUserID(), user);
+            System.out.println("User logged in successfully.");
+        } else {
+            System.out.println("Invalid username or password.");
+        }
+    }
+
+}
