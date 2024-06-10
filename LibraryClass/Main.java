f54194c228d665709463f8c6249d267a5e869267
Omid Sargazi <om.sargazi@gmail.com>
2024-06-10 18:57:32 +0330

    create a user class

---
+++ LibraryClass/Main.java
@@ -0,0 +1,8 @@
+package LibraryClass;
+
+public class Main {
+    public static void main(String[] args) {
+        User user1 = new User("omid", "om.@gmail.com", "258");
+        System.out.println(user1);
+    }
+}
---
+++ LibraryClass/User.java
@@ -0,0 +1,146 @@
+package LibraryClass;
+
+public class User {
+    private static int idCounter = 1;
+
+    private int userID;
+    private String username;
+    private String email;
+    private String password;
+    private String firstName;
+    private String lastName;
+    private String role;
+    private boolean isActive;
+    private boolean isVerified;
+    private String phoneNumber;
+    private String preferences;
+    private String verificationCode;
+
+    public User(String username, String email, String password) {
+        this.username = username;
+        this.userID = idCounter++;
+        this.email = email;
+        this.password = password;
+        this.role = "member";
+        this.isActive = true;
+        this.isVerified = true;
+    }
+
+    public static int getIdCounter() {
+        return idCounter;
+    }
+
+    public static void setIdCounter(int idCounter) {
+        User.idCounter = idCounter;
+    }
+
+    public int getUserID() {
+        return userID;
+    }
+
+    public void setUserID(int userID) {
+        this.userID = userID;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public boolean isActive() {
+        return isActive;
+    }
+
+    public void setActive(boolean isActive) {
+        this.isActive = isActive;
+    }
+
+    public boolean isVerified() {
+        return isVerified;
+    }
+
+    public void setVerified(boolean isVerified) {
+        this.isVerified = isVerified;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getPreferences() {
+        return preferences;
+    }
+
+    public void setPreferences(String preferences) {
+        this.preferences = preferences;
+    }
+
+    public String getVerificationCode() {
+        return verificationCode;
+    }
+
+    public void setVerificationCode(String verificationCode) {
+        this.verificationCode = verificationCode;
+    }
+
+    public String toString() {
+        return "User{" +
+                "userID=" + userID +
+                ", username='" + username + '\'' +
+                ", email='" + email + '\'' +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", role='" + role + '\'' +
+                ", isActive=" + isActive +
+                ", isVerified=" + isVerified +
+                '}';
+    }
+
+}
