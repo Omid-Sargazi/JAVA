656452070c24bb4bf962945f148cab53d0e2c4fb
Omid Sargazi <om.sargazi@gmail.com>
2024-06-12 02:02:59 +0330

    create Customer class

---
+++ OnlineStore/Customer.java
@@ -0,0 +1,11 @@
+package OnlineStore;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Customer extends User {
+
+    public Customer(String name, String email, String password) {
+        super(name, email, password);
+    }
+}
