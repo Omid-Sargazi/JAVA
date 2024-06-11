c432afe905a095f6df04ab91ec497f46e251dc67
Omid Sargazi <om.sargazi@gmail.com>
2024-06-12 01:50:06 +0330

    User Class (Base class for Customer and Stockholder)

---
+++ OnlineStore/User.java
@@ -0,0 +1,51 @@
+package OnlineStore;
+
+import java.util.UUID;
+
+public abstract class User {
+    private String name;
+    private String id;
+    private String email;
+    private String password;
+
+    public User(String name, String email, String password) {
+        this.id = UUID.randomUUID().toString();
+        this.name = name;
+        this.email = email;
+        this.password = password;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public boolean login(String email, String password) {
+        return this.email.equals(email) && this.password.equals(password);
+    }
+
+    public void logout() {
+        System.out.println(name + " logged out.");
+    }
+
+    public void changePassword(String oldPassword, String newPassword) {
+        if (this.password.equals(oldPassword)) {
+            this.password = newPassword;
+            System.out.println("Password changed successfully.");
+        } else {
+            System.out.println("Old password is incorrect.");
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "User{id='" + id + "', name='" + name + "', email='" + email + "'}";
+    }
+}
