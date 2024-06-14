c183cd72578dbd2c9addca3f3d75fcec4bfba379
Omid Sargazi <om.sargazi@gmail.com>
2024-06-14 12:51:22 +0330

    Vendor Class extends User

---
+++ OnlineStore02/Vendor.java
@@ -0,0 +1,7 @@
+package OnlineStore02;
+
+public class Vendor extends User {
+    public Vendor(String userId, String name, String email, String password) {
+        super(userId, name, email, password);
+    }
+}
