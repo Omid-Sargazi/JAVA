829cb081c6ed7bff776a8792c4a0b34272ef8e0a
Omid Sargazi <om.sargazi@gmail.com>
2024-06-14 12:45:16 +0330

    User class  in onlineStore

---
+++ OnlineStore02/Main.java
@@ -0,0 +1,8 @@
+package OnlineStore02;
+
+public class Main {
+    public static void main(String[] args) {
+        User user = new User("001", "John", "o@o.com", "123456");
+        System.out.println("user: " + user);
+    }
+}
