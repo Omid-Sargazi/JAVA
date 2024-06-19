c9b82eddcd6d48df50f4a07b318e7b08cde303f9
Omid Sargazi <om.sargazi@gmail.com>
2024-06-20 02:00:05 +0330

    create user abstract class and member and Librarian

---
+++ MainLibrarySOLID/Librarian.java
@@ -0,0 +1,7 @@
+package MainLibrarySOLID;
+
+public class Librarian extends User {
+    public Librarian(String id, String name, String email, String password) {
+        super(id, name, email, password);
+    }
+}
---
+++ MainLibrarySOLID/Member.java
@@ -0,0 +1,7 @@
+package MainLibrarySOLID;
+
+public class Member extends User {
+    public Member(String id, String name, String email, String password) {
+        super(id, name, email, password);
+    }
+}
