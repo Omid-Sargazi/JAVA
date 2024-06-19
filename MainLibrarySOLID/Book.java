06dc79de638e8b05d4850afaa8099c5a64a11fa8
Omid Sargazi <om.sargazi@gmail.com>
2024-06-20 02:14:23 +0330

    Book class

---
+++ MainLibrarySOLID/Book.java
@@ -0,0 +1,48 @@
+package MainLibrarySOLID;
+
+public class Book {
+    private String id;
+    private String title;
+    private String author;
+    private boolean isAvailable;
+
+    public Book(String id, String title, String author) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+        this.isAvailable = true;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public boolean isAvailable() {
+        return isAvailable;
+    }
+
+    public void setAvailable(boolean isAvailable) {
+        this.isAvailable = isAvailable;
+    }
+
+}
