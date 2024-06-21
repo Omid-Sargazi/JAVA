4250366dfecc8a639581fdc11ffae6b939b0305e
Omid Sargazi <om.sargazi@gmail.com>
2024-06-21 22:32:44 +0330

    create user and userService class

--- MainLibrarySOLID/User.java
+++ MainLibrarySOLID/User.java
@@ -1,52 +1,21 @@
 package MainLibrarySOLID;
 
-public abstract class User {
-    private String id;
+public class User {
+    private String userId;
     private String name;
     private String email;
     private String password;
+    private List<Loan> borrowingHistory;
 
-    public User(String id, String name, String email, String password) {
-        this.id = id;
+    // Constructor
+    public User(String userId, String name, String email, String password) {
+        this.userId = userId;
         this.name = name;
         this.email = email;
         this.password = password;
+        this.borrowingHistory = new ArrayList<>();
     }
 
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public boolean checkPassword(String password) {
-        return this.password.equals(password);
-    }
-
+    // Getters and Setters
+    // Other methods like updateProfile, changePassword, etc.
 }
