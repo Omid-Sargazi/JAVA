9c08084f6b690912143b500913c59658041193bc
Omid Sargazi <om.sargazi@gmail.com>
2024-06-21 17:16:00 +0330

    correct book class

--- MainLibrarySOLID/Book.java
+++ MainLibrarySOLID/Book.java
@@ -1,48 +1,21 @@
 package MainLibrarySOLID;
 
 public class Book {
-    private String id;
+    private String isbn;
     private String title;
     private String author;
+    private String genre;
     private boolean isAvailable;
 
-    public Book(String id, String title, String author) {
-        this.id = id;
+    // Constructor
+    public Book(String isbn, String title, String author, String genre) {
+        this.isbn = isbn;
         this.title = title;
         this.author = author;
+        this.genre = genre;
         this.isAvailable = true;
     }
 
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getAuthor() {
-        return author;
-    }
-
-    public void setAuthor(String author) {
-        this.author = author;
-    }
-
-    public boolean isAvailable() {
-        return isAvailable;
-    }
-
-    public void setAvailable(boolean isAvailable) {
-        this.isAvailable = isAvailable;
-    }
-
+    // Getters and Setters
+    // Other methods
 }
