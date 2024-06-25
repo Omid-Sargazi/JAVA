3a7e441cf57053c1f1508d457b4b3c9fbbbbb0a3
Omid-Saegazi <om.sargazi@gmail.com>
2024-06-25 13:58:09 +0330

    Area Calculator without OCP

---
+++ SOLID/RectangleOCP/Rectangle.java
@@ -0,0 +1,24 @@
+package SOLID.RectangleOCP;
+
+class Rectangle {
+    public double length;
+    public double width;
+
+}
+
+class Circle {
+    public double radius;
+}
+
+class AreaCalculator {
+    public double calculateArea(Object shape) {
+        if (shape instanceof Rectangle) {
+            Rectangle rectangle = (Rectangle) shape;
+            return rectangle.length * rectangle.width;
+        } else if (shape instanceof Circle) {
+            Circle circle = (Circle) shape;
+            return Math.PI * circle.radius * circle.radius;
+        }
+        return 0;
+    }
+}
