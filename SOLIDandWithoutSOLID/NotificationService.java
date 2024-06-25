53b4db97913bd303ccc859d5fa1da8f476f1983e
Omid-Saegazi <om.sargazi@gmail.com>
2024-06-25 12:58:12 +0330

    send Notification without SOLID principles

---
+++ SOLIDandWithoutSOLID/NotificationService.java
@@ -0,0 +1,27 @@
+package SOLIDandWithoutSOLID;
+
+public class NotificationService {
+    public void sendNotification(String type, String message) {
+        if (type.equals("email")) {
+            sendEmail(message);
+        } else if (type.equals("sma")) {
+            sendSMS(message);
+        } else if (type.equals("push")) {
+            sendPushNotification(message);
+        } else {
+            throw new IllegalArgumentException("Unknown notification type");
+        }
+    }
+
+    private void sendEmail(String message) {
+        System.out.println("Sending email with message: " + message);
+    }
+
+    private void sendSMS(String message) {
+        System.out.println("Sending SMS with message: " + message);
+    }
+
+    private void sendPushNotification(String message) {
+        System.out.println("Sending push notification with message: " + message);
+    }
+}
