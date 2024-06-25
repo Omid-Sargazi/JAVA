edb1e05ce40a8d50d3a5a43f79b99d3b86a2ca97
Omid-Saegazi <om.sargazi@gmail.com>
2024-06-26 00:51:34 +0330

    class worker without ISP principles

---
+++ SOLID/WorkerISP/HumanWorker.java
@@ -0,0 +1,13 @@
+package SOLID.WorkerISP;
+
+public class HumanWorker implements Worker {
+    @Override
+    public void work() {
+        System.out.println("Human working");
+    }
+
+    @Override
+    public void eat() {
+        System.out.println("Human eating");
+    }
+}
---
+++ SOLID/WorkerISP/RobotWorker.java
@@ -0,0 +1,13 @@
+package SOLID.WorkerISP;
+
+public class RobotWorker implements Worker {
+    @Override
+    public void work() {
+        System.out.println("Robot working");
+    }
+
+    @Override
+    public void eat() {
+        throw new UnsupportedOperationException("Robots don't eat");
+    }
+}
