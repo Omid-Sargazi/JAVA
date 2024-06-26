bf3e50f80be5182d7a96e932f2bc7d5b0a221bc9
Omid-Saegazi <om.sargazi@gmail.com>
2024-06-27 00:48:53 +0330

    Aggregation - Library Composition

---
+++ OOPLibrary/Library.java
@@ -0,0 +1,33 @@
+package OOPLibrary;
+
+import java.util.List;
+import java.util.ArrayList;
+
+public class Library {
+    private List<Book> books;
+
+    public Library() {
+        this.books = new ArrayList<>();
+    }
+
+    public void addBook(Book book) {
+        books.add(book);
+    }
+
+    public void removeBook(Book book) {
+        books.remove(book);
+    }
+
+    public List<Book> getBooks() {
+        return books;
+    }
+
+    public Book findBookByTitle(String title) {
+        for (Book book : books) {
+            if (book.getTitle().equals(title)) {
+                return book;
+            }
+        }
+        return null;
+    }
+}
