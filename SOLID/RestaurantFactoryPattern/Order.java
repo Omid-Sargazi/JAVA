36ddba829c50f096a686f089d175041cf35817b6
Omid-Saegazi <om.sargazi@gmail.com>
2024-06-28 12:54:04 +0330

    Create Order Class

---
+++ SOLID/RestaurantFactoryPattern/Order.java
@@ -0,0 +1,22 @@
+package SOLID.RestaurantFactoryPattern;
+
+import java.util.Date;
+
+public class Order {
+    private User customer;
+    private Food food;
+    private Date orderDate;
+
+    public Order(User customer, Food food) {
+        this.customer = customer;
+        this.food = food;
+        this.orderDate = new Date();
+    }
+
+    public void showOrderDetails() {
+        customer.showDetails();
+        food.prepare();
+        System.out.println("Order Date: " + orderDate);
+
+    }
+}
