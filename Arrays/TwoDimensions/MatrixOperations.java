17fd580a288e7436be66b1dbbfead63fb9a25b72
Omid-Saegazi <om.sargazi@gmail.com>
2024-07-01 01:13:17 +0330

    transpose Matrix

--- Arrays/TwoDimensions/MatrixOperations.java
+++ Arrays/TwoDimensions/MatrixOperations.java
@@ -46,6 +46,17 @@ public class MatrixOperations {
         }
     }
 
+    public static int[][] transposeMatrix(int[][] matrix) {
+        int[][] transposeMatrix = new int[matrix[0].length][matrix.length];
+
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                transposeMatrix[j][i] = matrix[i][j];
+            }
+        }
+        return transposeMatrix;
+    }
+
     public static void main(String[] args) {
         int rows = 5;
         int cols = 5;
