e2d304699f8cb934350aa85dc1401ac228717ef6
Omid-Saegazi <om.sargazi@gmail.com>
2024-07-01 01:25:13 +0330

    Concrete Hospital Manager

---
+++ HospitalManagemenFactory/ConcreteHospitalManager.java
@@ -0,0 +1,17 @@
+package HospitalManagemenFactory;
+
+public class ConcreteHospitalManager extends HospitalManager {
+
+    @Override
+    public MedicalStaff createStaff(String type) {
+        switch (type.toLowerCase()) {
+            case "doctor":
+                return new Doctor();
+            case "nurse":
+                return new Nurse();
+
+            default:
+                throw new IllegalArgumentException("Unknown staff type");
+        }
+    }
+}
---
+++ HospitalManagemenFactory/HospitalManager.java
@@ -0,0 +1,13 @@
+package HospitalManagemenFactory;
+
+public abstract class HospitalManager {
+    public abstract MedicalStaff createStaff(String type);
+
+    public void manage() {
+        MedicalStaff doctor = createStaff("doctor");
+        doctor.performDuty();
+
+        MedicalStaff nurse = createStaff("nurse");
+        nurse.performDuty();
+    }
+}
