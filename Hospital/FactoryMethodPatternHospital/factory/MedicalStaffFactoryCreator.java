8bcda2a62c1bb5dcffa41ac7faaf381e9480249a
Omid-Saegazi <om.sargazi@gmail.com>
2024-07-03 00:13:15 +0330

    Medical Staff Factory Creator

---
+++ Hospital/FactoryMethodPatternHospital/factory/MedicalStaffFactoryCreator.java
@@ -0,0 +1,20 @@
+package Hospital.FactoryMethodPatternHospital.factory;
+
+public class MedicalStaffFactoryCreator {
+    public static MedicalStaffFactory getFactory(String type)
+    {
+        switch (type.toLowerCase()) {
+            case "doctor":
+            return new DoctorFactory()    
+            
+            case "nurse":
+            return new NurseFactory();
+
+            case "surgeon":
+            return new SurgeonFactory();
+        
+            default:
+            throw new IllegalArgumentException("Unknown medical staff type");
+        }
+    }
+}
