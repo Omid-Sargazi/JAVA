63000fc7ebe867768e839cd5929583fdae6252e0
Omid-Saegazi <om.sargazi@gmail.com>
2024-07-02 18:55:29 +0330

    Hospital (Composition)

---
+++ Hospital/entity/Hospital.java
@@ -0,0 +1,29 @@
+package Hospital.entity;
+
+import java.util.List;
+
+public class Hospital {
+    private String name;
+    private List<Department> departments;
+
+    public Hospital(String name, List<Department> departments) {
+        this.name = name;
+        this.departments = departments;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public List<Department> gDepartments() {
+        return departments;
+    }
+
+    public void showDetails() {
+        System.out.println("Hospital [Name=" + name + "]");
+
+        for (Department department : departments) {
+            department.showDetails();
+        }
+    }
+}
